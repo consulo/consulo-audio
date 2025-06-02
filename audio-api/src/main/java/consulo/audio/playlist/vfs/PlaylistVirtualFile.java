86a78248957ff7e17db833d1313c1e712c533c82
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-06-02 15:08:08 +0400

    compile fix

--- audio-api/src/main/java/consulo/audio/playlist/vfs/PlaylistVirtualFile.java
+++ audio-api/src/main/java/consulo/audio/playlist/vfs/PlaylistVirtualFile.java
@@ -2,34 +2,34 @@ package consulo.audio.playlist.vfs;
 
 import consulo.language.file.light.LightVirtualFile;
 import consulo.virtualFileSystem.VirtualFileSystem;
-
 import jakarta.annotation.Nonnull;
 
 /**
  * @author VISTALL
  * @since 17/12/2020
  */
-public class PlaylistVirtualFile extends LightVirtualFile
-{
-	@Nonnull
-	private final VirtualFileSystem myVirtualFileSystem;
+public class PlaylistVirtualFile extends LightVirtualFile {
+    @Nonnull
+    private final VirtualFileSystem myVirtualFileSystem;
 
-	public PlaylistVirtualFile(@Nonnull VirtualFileSystem virtualFileSystem)
-	{
-		super("Playlist");
-		myVirtualFileSystem = virtualFileSystem;
-	}
+    public PlaylistVirtualFile(@Nonnull VirtualFileSystem virtualFileSystem) {
+        super("Playlist");
+        myVirtualFileSystem = virtualFileSystem;
+    }
 
-	@Nonnull
-	@Override
-	public VirtualFileSystem getFileSystem()
-	{
-		return myVirtualFileSystem;
-	}
+    @Nonnull
+    @Override
+    public VirtualFileSystem getFileSystem() {
+        return myVirtualFileSystem;
+    }
 
-	@Override
-	public boolean equals(Object obj)
-	{
-		return obj instanceof PlaylistVirtualFile;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof PlaylistVirtualFile;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
 }
--- audio-api/src/main/java/consulo/audio/playlist/vfs/PlaylistVirtualFileSystem.java
+++ audio-api/src/main/java/consulo/audio/playlist/vfs/PlaylistVirtualFileSystem.java
@@ -3,8 +3,6 @@ package consulo.audio.playlist.vfs;
 import consulo.annotation.component.ExtensionImpl;
 import consulo.virtualFileSystem.BaseVirtualFileSystem;
 import consulo.virtualFileSystem.VirtualFile;
-import consulo.virtualFileSystem.VirtualFileManager;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
@@ -13,40 +11,28 @@ import jakarta.annotation.Nullable;
  * @since 17/12/2020
  */
 @ExtensionImpl
-public class PlaylistVirtualFileSystem extends BaseVirtualFileSystem
-{
-	@Nonnull
-	public static PlaylistVirtualFileSystem getInstance()
-	{
-		return (PlaylistVirtualFileSystem) VirtualFileManager.getInstance().getFileSystem(PROTOCOL);
-	}
+public class PlaylistVirtualFileSystem extends BaseVirtualFileSystem {
+    public static final String PROTOCOL = "audiolist";
 
-	public static final String PROTOCOL = "audiolist";
+    @Nonnull
+    @Override
+    public String getProtocol() {
+        return PROTOCOL;
+    }
 
-	@Nonnull
-	@Override
-	public String getProtocol()
-	{
-		return PROTOCOL;
-	}
+    @Nullable
+    @Override
+    public VirtualFile findFileByPath(@Nonnull String path) {
+        return new PlaylistVirtualFile(this);
+    }
 
-	@Nullable
-	@Override
-	public VirtualFile findFileByPath(@Nonnull String path)
-	{
-		return new PlaylistVirtualFile(this);
-	}
+    @Override
+    public void refresh(boolean b) {
+    }
 
-	@Override
-	public void refresh(boolean b)
-	{
-
-	}
-
-	@Nullable
-	@Override
-	public VirtualFile refreshAndFindFileByPath(@Nonnull String path)
-	{
-		return findFileByPath(path);
-	}
+    @Nullable
+    @Override
+    public VirtualFile refreshAndFindFileByPath(@Nonnull String path) {
+        return findFileByPath(path);
+    }
 }
